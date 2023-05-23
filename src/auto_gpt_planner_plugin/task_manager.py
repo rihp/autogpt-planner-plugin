7bfa4c5786978e1cbea1e189d2d937c222fc865c
brucemeek <113046530+brucemeek@users.noreply.github.com>
2023-05-22 20:48:04 -0500

    more updates

--- src/auto_gpt_planner_plugin/task_manager.py
+++ src/auto_gpt_planner_plugin/task_manager.py
@@ -33,35 +33,41 @@ class TaskManager:
         with open(file_name, "w") as f:
             json.dump(self.tasks, f, indent=4)
 
-    def add_task(self, task_description, deadline=None, priority=None, assignee=None):
+    def add_task(self, task_id=None, task_description=None, deadline=None, priority=None, assignee=None):
         """Adds a task to the task manager."""
         task = {
-            "task_id": self.task_counter,  # Modify this line
-            "task_description": task_description,
+            "task_id": task_id if task_id is not None else self.task_counter,
+            "task_description": task_description if task_description is not None else "No description",
             "completed": False,
-            "deadline": deadline,
-            "priority": priority,
-            "assignee": assignee,
+            "deadline": deadline if deadline is not None else "No deadline",
+            "priority": priority if priority is not None else "No priority",
+            "assignee": assignee if assignee is not None else "No assignee",
             "progress": 0
         }
         self.validate_task(task)
         self.tasks.append(task)
-        self.save_tasks()  # Save tasks after adding a new one
+        self.save_tasks()
         self.task_counter += 1
+
+    def add_tasks(self, task_list):
+        """Adds multiple tasks to the task manager."""
+        for task in task_list:
+            if not task.get('completed', False):
+                self.add_task(**task)
 
     def update_task_status(self, task_id, **kwargs):
         """Updates the status of a task in the task manager."""
         for task in self.tasks:
             if task["task_id"] == task_id:
                 task.update(kwargs)
-        self.save_tasks()  # Save tasks after updating a task's status
+        self.save_tasks()
 
     def delete_task(self, task_id):
         """Deletes a task from the task manager."""
         for task in self.tasks:
             if task["task_id"] == task_id:
                 self.tasks.remove(task)
-        self.save_tasks()  # Save tasks after deleting a task
+        self.save_tasks()
 
     def generate_report(self):
         """Generates a report of the tasks."""
@@ -83,3 +89,35 @@ class TaskManager:
             report["tasks_by_assignee"][task["assignee"]] += 1
 
         return report
+
+def validate_task(self, task):
+    """Validates a task before adding it to the task manager."""
+    required_fields = ["task_id", "task_description", "completed", "progress"]
+
+    for field in required_fields:
+        if field not in task:
+            raise ValueError(f"Task is missing {field}")
+
+    if not isinstance(task["task_id"], int):
+        raise ValueError("Task ID must be an integer")
+
+    if not isinstance(task["task_description"], str):
+        raise ValueError("Task description must be a string")
+
+    if not isinstance(task["completed"], bool):
+        raise ValueError("Task completion status must be a boolean")
+
+    if not isinstance(task["progress"], int) or not (0 <= task["progress"] <= 100):
+        raise ValueError("Task progress must be an integer between 0 and 100")
+
+    if task["deadline"] is not None:
+        try:
+            datetime.strptime(task["deadline"], "%Y-%m-%d")
+        except ValueError:
+            raise ValueError("Task deadline must be a string in the format 'YYYY-MM-DD'")
+
+    if task["priority"] is not None and task["priority"] not in ["high", "medium", "low", None]:
+        raise ValueError("Task priority must be 'high', 'medium', 'low', or None")
+
+    if task["assignee"] is not None and not isinstance(task["assignee"], str):
+        raise ValueError("Task assignee must be a string")
--- src/auto_gpt_planner_plugin/utils.py
+++ src/auto_gpt_planner_plugin/utils.py
@@ -1,4 +1,5 @@
 import tokenizers
+import re
 
 # Global constants
 tokenizer = tokenizers.ByteLevelBPETokenizer()
@@ -13,18 +14,27 @@ def process_response(response):
         list: A list of response chunks.
     """
     MAX_TOKENS = 4096
+    CONTEXT_MARKER = "<|context|>"
     tokens = tokenizer.encode(response)
+    
     if len(tokens) > MAX_TOKENS:
         chunks = []
         current_chunk_tokens = []
+        
         for token in tokens:
             if len(current_chunk_tokens) + 1 <= MAX_TOKENS:
                 current_chunk_tokens.append(token)
             else:
+                # Add contextual marker at the end of the chunk
+                current_chunk_tokens.append(tokenizer.encode(CONTEXT_MARKER))
                 chunks.append(tokenizer.decode(current_chunk_tokens))
-                current_chunk_tokens = [token]
+                
+                # Add contextual marker at the beginning of the next chunk
+                current_chunk_tokens = [tokenizer.encode(CONTEXT_MARKER), token]
+        
         if current_chunk_tokens:
             chunks.append(tokenizer.decode(current_chunk_tokens))
+        
         return chunks
     else:
         return [response]
